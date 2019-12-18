525900f6ed5d48a0ea226dee02c205a27182e5d1
nnnkit <iankit17@gmail.com>
2019-12-18 10:23:37 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// Absolute Imports
+import React from "react";
+import { render } from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Relative Imports
+import "./styles/normalize.css";
+import "./styles/style.css";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+export default function Index() {
+  return <div>Hello World!</div>;
+}
+
+render(<Index />, document.getElementById("root"));
